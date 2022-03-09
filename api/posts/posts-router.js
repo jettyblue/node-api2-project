e6f242f11569419383444c2c1bf6180aeb81cd9c
unknown <lindsay.kelly15@gmail.com>
2022-03-08 19:26:58 -0500

    express installed, server listening

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,15 @@
 // implement your posts router here
+const express = require('express');
+const { restart } = require('nodemon');
+const Post = require('./posts-model');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The posts information could not be retrieved" });
+        })
+})
